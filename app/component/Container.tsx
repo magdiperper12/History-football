6f5f6fa843335d1e1082be2d88d2078f82196375
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2024-11-14 02:58:20 +0200

    init

--- app/component/Container.tsx
+++ app/component/Container.tsx
@@ -50,7 +50,7 @@ const Container: React.FC = () => {
 			{/* Football Match Results */}
 			<div>
 				<h2 className='text-2xl font-semibold m-4 flex justify-center'>
-					Football Match Results
+					محدش مهتم يسمع قصتك غير لما توصل
 				</h2>
 				<div className='flex flex-col w-full justify-center gap-5'>
 					{matches.map((match, index) => (
